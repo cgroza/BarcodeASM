48413e692cf3a23c50d5c7a4a7c8c2bd416161d6
Cristian Groza <cgroza@mallow.local>
2020-07-21 19:35:56 -0400

    Trim before simplifying.

--- src/bxlra/LocalAssemblyWindow.cpp
+++ src/bxlra/LocalAssemblyWindow.cpp
@@ -151,25 +151,19 @@ void LocalAssemblyWindow::assembleFromGraph(std::stringstream &asqg_stream,
   // removes redundant paths from the graph
   str_graph->visit(trans_visitor);
 
-  str_graph->writeASQG(m_prefix + "_trans_1_graph.asqg");
-
-  str_graph->simplify(); // merges vertices by removing transitive edges
-
-  str_graph->writeASQG(m_prefix + "_simplify_2_graph.asqg");
-
-  if (m_params.validate)
-    str_graph->visit(validate_visitor);
-
   // Remove branches that do not merge into to form a bubble
-
   if (m_params.perform_trim) {
     for (size_t i = 0; i < m_params.trim_rounds; i++)
       str_graph->visit(trim_visitor);
   }
-  str_graph->writeASQG(m_prefix + "_trim_3_graph.asqg");
+
+  str_graph->simplify(); // merges vertices by removing transitive edges
+
+  if (m_params.validate)
+    str_graph->visit(validate_visitor);
 
   // identify these vertices with a unique prefix
-  str_graph->renameVertices(m_prefix);
+  str_graph->renameVertices(m_prefix + "_");
 
   // walk graph to retrieve contigs
   walkAssemblyGraph(str_graph);
