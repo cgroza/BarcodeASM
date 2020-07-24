de19054e7cc977d5a75b870c389f307750295083
Cristian Groza <cgroza@mallow.local>
2020-07-23 21:25:29 -0400

    Cmd line params for trim rounds and trim length.

--- src/bxlra/bxlra.cpp
+++ src/bxlra/bxlra.cpp
@@ -56,6 +56,8 @@ std::string regions_path;
 std::string reference_path;
 size_t min_overlap = 90;
 size_t num_threads = 1;
+size_t trim_rounds = 3;
+size_t min_branch_len;
 bool weird_reads_only = true;
 } // namespace opt
 
@@ -74,7 +76,7 @@ int main(int argc, char **argv) {
 
   opterr = 0;
   int c;
-  while ((c = getopt(argc, argv, "at:b:B:r:g:o:")) != -1)
+  while ((c = getopt(argc, argv, "at:b:B:r:g:o:T:l:")) != -1)
     switch (c) {
     case 't':
         try {
@@ -103,11 +105,20 @@ int main(int argc, char **argv) {
     case 'o':
       opt::min_overlap = std::stoi(optarg);
       break;
+    case 'l':
+      opt::min_branch_len = std::stoi(optarg);
+      break;
+    case 'T':
+      opt::trim_rounds = std::stoi(optarg);
+      break;
     default:
       abort();
     }
+
   AssemblyParams params;
   params.min_overlap = opt::min_overlap;
+  params.trim_length_threshold = opt::min_branch_len;
+  params.trim_rounds = opt::trim_rounds;
 
   // Storage for thread pooled resources
   // These not be guarded by mutex, since they assigned to individual thread IDs
@@ -139,10 +150,6 @@ int main(int argc, char **argv) {
   // Regions to be locally assembled
   RegionFileReader region_reader(opt::regions_path, bam_readers[0]->Header());
 
-  // Stores output from each thread, must be protected by mutex
-  std::vector<std::pair<LocalAssemblyWindow*, LocalAlignment*>> output;
-  std::mutex output_mutex;
-
   // file to write contig sequences in
   std::ofstream fasta("contigs.fa");
   std::mutex fasta_mutex;
@@ -156,49 +163,36 @@ int main(int argc, char **argv) {
   for (auto region : region_reader.getRegions()) {
     std::string chrom = region.ChrName(bam_readers[0]->Header());
     std::cerr << "Running " << chrom << " " << region.pos1 << " " << region.pos2 << std::endl;
-    auto future = thread_pool.push([region, &output, &output_mutex, &fasta,
-                                    &fasta_mutex, &alns, &alns_mutex, &params,
+    auto future = thread_pool.push([region, &fasta, &fasta_mutex,
+                                    &alns, &alns_mutex, &params,
                                     &ref_genomes, &bam_readers,
                                     &bx_bam_walkers](int id) {
 
       std::cerr << "ID " << id << std::endl;
-      LocalAssemblyWindow *local_win = new LocalAssemblyWindow(region, *bam_readers[id], *bx_bam_walkers[id], params);
+      LocalAssemblyWindow local_win(region, *bam_readers[id], *bx_bam_walkers[id], params);
 
-      local_win -> assembleReads();
-      local_win -> clearReads();
-      std::cerr << "Reads: " << local_win -> getReads().size() << std::endl;
-      std::cerr << "Contigs: " << local_win -> getContigs().size() << std::endl;
+      local_win.assembleReads();
+      local_win.clearReads();
+      std::cerr << "Reads: " << local_win.getReads().size() << std::endl;
+      std::cerr << "Contigs: " << local_win.getContigs().size() << std::endl;
 
       // MUTEX: only one thread must write to the fasta file at a time
       fasta_mutex.lock();
-      local_win -> writeContigs(fasta);
+      local_win.writeContigs(fasta);
       fasta_mutex.unlock();
 
-      LocalAlignment *local_alignment = new LocalAlignment(region.ChrName(bam_readers[0]->Header()),
-                                                           region.pos1, region.pos2, *ref_genomes[id]);
-      local_alignment -> align(local_win -> getContigs());
+      LocalAlignment local_alignment(region.ChrName(bam_readers[0]->Header()),
+                                      region.pos1, region.pos2, *ref_genomes[id]);
+      local_alignment.align(local_win.getContigs());
 
       // MUTEX: only one thread must write to alignments
       alns_mutex.lock();
-      local_alignment -> writeAlignments(alns);
+      local_alignment.writeAlignments(alns);
       alns_mutex.unlock();
-
-      // MUTEX: only one thread must push to std::vector
-      output_mutex.lock();
-      output.push_back(std::pair<LocalAssemblyWindow*, LocalAlignment*>(local_win, local_alignment));
-      output_mutex.unlock();
     });
   }
 
   thread_pool.stop(true);
   fasta.close();
   alns.close();
-
-  // cleanup allocated memory
-  for (auto &region_output : output) {
-      delete region_output.first;
-      delete region_output.second;
-  }
-
-  output.clear();
 }
