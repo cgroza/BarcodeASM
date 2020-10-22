fe97030e47cf7381a4d5d4081665c6c448ea8027
Cristian Groza <cgroza@mallow.local>
2020-10-22 15:03:05 -0400

    Sort sequences by locus and length when filtering.

--- scripts/filter_hash.py
+++ scripts/filter_hash.py
@@ -1,5 +1,6 @@
 import pysam
 import argparse
+import functools
 
 
 parser = argparse.ArgumentParser("Reduce out contigs with redundant non-reference sequences.")
@@ -15,14 +16,28 @@ reduced_fasta = open(args.out[0], "w")
 
 hashes = set()
 
-for seq_name in contig_fasta.references:
+def cmp_contig(x, y):
+    (x_chrom, x_start, _, _, _, _, _, _, x_sv_len) = x.split("_")
+    (y_chrom, y_start, _, _, _, _, _, _, y_sv_len) = y.split("_")
+
+    x_start, x_sv_len, y_start, y_sv_len = int(x_start), int(x_sv_len), int(y_start), int(y_sv_len)
+
+    if x_chrom == y_chrom:
+        if x_start == y_start:
+            if x_sv_len < y_sv_len: return 1
+            else: return -1
+        if x_start < y_start: return -1
+        else: return 1
+    elif x_chrom < y_chrom: return -1
+    else: return 1
+
+for seq_name in sorted(contig_fasta.references, key=functools.cmp_to_key(cmp_contig)):
     print(seq_name)
     (chrom, start, end, ps, hp, num, sample, var_hash, sv_len) = seq_name.split("_")
     if var_hash not in hashes:
         # output unique variant
         contig_seq = contig_fasta.fetch(seq_name)
         reduced_fasta.writelines([">" + seq_name + "\n", contig_seq + "\n"])
-
         # never output this hash again
         hashes.add(var_hash)
 
